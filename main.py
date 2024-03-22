9e69a5a8cfe039b05f00212eb8c75d97df40c05a
Toan <phanqu6ctoan@gmail.com>
2024-03-22 17:46:38 +0700

    Refresh git cache

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 from selenium.common import TimeoutException
 from selenium.webdriver.chrome.options import Options
 import os
-from time import sleep
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import csv
@@ -78,7 +77,7 @@ def loginFacebookByCookie(driver, cookie):
         if cookie is not None:
             script = 'javascript:void(function(){ function setCookie(t) { var list = t.split("; "); console.log(list); for (var i = list.length - 1; i >= 0; i--) { var cname = list[i].split("=")[0]; var cvalue = list[i].split("=")[1]; var d = new Date(); d.setTime(d.getTime() + (7*24*60*60*1000)); var expires = ";domain=.facebook.com;expires="+ d.toUTCString(); document.cookie = cname + "=" + cvalue + "; " + expires; } } function hex2a(hex) { var str = ""; for (var i = 0; i < hex.length; i += 2) { var v = parseInt(hex.substr(i, 2), 16); if (v) str += String.fromCharCode(v); } return str; } setCookie("' + cookie + '"); location.href = "https://mbasic.facebook.com"; })();'
             driver.execute_script(script)
-            sleep(5)
+            time.sleep(5)
     except:
         print("loi login")
 
